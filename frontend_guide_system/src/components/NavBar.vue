3fdbaab157769a727f1e587b73bc00c630cb478e
wsz <1175690167@qq.com>
2022-10-20 23:46:47 +0800

    fix:增加底部安全区适配

--- frontend_guide_system/src/components/NavBar.vue
+++ frontend_guide_system/src/components/NavBar.vue
@@ -1,5 +1,5 @@
 <template>
-    <van-tabbar route>
+    <van-tabbar route class="van-safe-area-bottom">
         <van-tabbar-item replace to="/" icon="home-o">首页</van-tabbar-item>
         <van-tabbar-item replace to="/help" icon="search">攻略</van-tabbar-item>
         <van-tabbar-item replace to="/user" icon="search">我的</van-tabbar-item>

743bebc2bb608aea8acf91891756c35e5d4e5196
wsz <1175690167@qq.com>
2022-10-31 02:14:00 +0800

    feat:完成[个人信息]界面搭建

--- frontend_guide_system/src/views/user/UserView.vue
+++ frontend_guide_system/src/views/user/UserView.vue
@@ -1,5 +1,61 @@
 <template>
     <van-nav-bar title="BJUT个人信息" />
+    <!-- 蓝色大背景 -->
+    <div class="container" style="background-color: #3d8af2; height: 90%; position: absolute; top: 1%%; width: 100%;">
+        <!-- 用户卡片 -->
+        <div class="user-card">
+            <div style="width: 100%; height:18%"></div>
+            <van-row justify="center" align="center">
+                <van-col span="4"></van-col>
+                <van-col span="6">
+                    <img src="../../assets/user_photo.jpeg"
+                        style="position: relative; height:80%; width:50%; border-radius:50%; justify-content: center; text-align: center;">
+                </van-col>
+                <van-col span="14" style="font-size: 20px; text-align: left; justify-content: left;">下午好，王思哲！</van-col>
+            </van-row>
+        </div>
+        <!-- 课程卡片 -->
+        <div class="user-courses">
+            <div style="position: relative; top:3%; left:4%; font-weight: bold;">我的课表</div>
+            <img src="../../assets/course.jpeg" style="position: relative; height:90%; width:100%; top: 7%;">
+        </div>
+    </div>
+    <!-- 偏好设置[待完成] -->
+    <div class="user-settings">
+        <van-button icon="idcard" type="primary" color="#3d8af2" block plain>
+            偏好设置
+        </van-button>
+    </div>
 </template>
 
 <script setup></script>
+
+<style scoped>
+.user-card {
+    background-color: white;
+    height: 12%;
+    width: 90%;
+    position: absolute;
+    top: 3%;
+    left: 5%;
+    border-radius: 5px;
+    line-height: 12%;
+}
+
+.user-courses {
+    background-color: white;
+    height: 60%;
+    width: 90%;
+    position: absolute;
+    top: 19%;
+    left: 5%;
+    border-radius: 5px;
+}
+
+.user-settings {
+    position: absolute;
+    top: 81%;
+    left: 5%;
+    width: 90%;
+}
+</style>

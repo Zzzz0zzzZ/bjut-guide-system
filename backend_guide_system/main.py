3bba4fdf8cab561c02fe077348e4815560c5283f
wsz <1175690167@qq.com>
2022-10-31 18:34:24 +0800

    feat:增加资源图片接口,算法接口

--- backend_guide_system/main.py
+++ backend_guide_system/main.py
@@ -11,6 +11,8 @@ from fastapi import FastAPI, Form
 from fastapi.responses import StreamingResponse
 from fastapi.middleware.cors import CORSMiddleware
 from algorithm.calcpath import CalcPath_DP
+from algorithm.autopath import AutoPather
+from algorithm.calcpath_aco import run_aco
 from funcs.getcourse import GetCourse
 
 # 实例化
@@ -49,10 +51,30 @@ def getUsers():
 def getPaths(chosen_list=Form(None)):
     # 接受表单数据，转换为list类型
     chosen_list =[int(x) for x in chosen_list.split(',')]
+    chosen_list_len = len(chosen_list)
+    # 如果选择地点多于15个点，将执行自动舍弃算法
+    if chosen_list_len > 15:
+        auto_pather = AutoPather(chosen_list=chosen_list)
+        chosen_list = auto_pather.model_forward()
     # 实例化
     alg = CalcPath_DP(chosen_list=chosen_list)
     # 运行算法 DP
     path_idx_list, path_num_list, path_length, path_between_list = alg.run()
+    # 返回json格式数据
+    return {
+        "path_idx_list": path_idx_list,
+        "path_num_list": path_num_list,
+        "path_length": path_length,
+        "path_between_list": path_between_list
+    }
+
+@app.post("/guide_aco")
+def getPaths(chosen_list=Form(None)):
+    # 接受表单数据，转换为list类型
+    chosen_list =[int(x) for x in chosen_list.split(',')]
+    # 执行算法
+    path_idx_list, path_num_list, path_length, path_between_list = run_aco(chosen_list=chosen_list)
+
     # 返回json格式数据
     return {
         "path_idx_list": path_idx_list,
@@ -80,6 +102,15 @@ async def download_files_stream():
         media_type="image/png"
     )
 
+#  返回图片
+@app.post("/result_aco")
+async def download_files_stream():
+    path_result_pic = open('data/res/path_result_aco.png', mode="rb")
+    return StreamingResponse(
+        path_result_pic,
+        media_type="image/png"
+    )
+
 @app.get("/next_course")
 def getNextCourse():
     getCourse = GetCourse()
@@ -91,6 +122,24 @@ def read_item(item_id: int):
     return {
         "item_id": item_id
     }
+
+#  返回用户头像
+@app.post("/user_photo")
+async def user_photo():
+    photo = open('data/pics/user_photo.png', mode="rb")
+    return StreamingResponse(
+        photo,
+        media_type="image/png"
+    )
+
+#  返回用户课程表
+@app.post("/course")
+async def course():
+    photo = open('data/pics/course.png', mode="rb")
+    return StreamingResponse(
+        photo,
+        media_type="image/png"
+    )
 
 if __name__ == '__main__':
     uvicorn.run(app)
